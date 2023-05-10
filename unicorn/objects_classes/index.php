68a069e2c4f0dca426b323fb0ff79342fdbd723e
NatasaIc <iconomou.gmail.com>
2023-05-10 10:20:16 +0200

    gotilla file

--- unicorn/objects_classes/index.php
+++ unicorn/objects_classes/index.php
@@ -1,4 +1,5 @@
 <?php 
+//PART 1-2
 // declare(strict_types=1);
 
 // class Unicorn {
@@ -30,6 +31,7 @@
 // $renderer->renderMessage($u);
 // $renderer->renderMessage($v);
 
+// PART 3-4
 // declare(strict_types=1);
 
 // class Unicorn {
@@ -72,56 +74,107 @@ declare(strict_types=1);
 
 class Unicorn {
     private $name = "";
-    private $color = "pink";
+    private $color = "";
     private $age = 0;
 
-    function __construct(string $name, string $color = "pink", int $age){
+    function __construct(string $name, string $color, int $age){
         $this->name = $name;
         $this->color = $color;
         $this->age = $age;
     }
 
     public function getMessage(): string{
-        return foreach($u as $unicorns){
-            "<p>{$this->name} says hello I am {$this->color} and {$this->age} years old!";)
-        }
+        return "<p>{$this->name} says hello I am {$this->color} and {$this->age} years old!</p>";
+    }
+
+    public function getName(): string {
+        return "<li>{$this->name}</li><br>";
+    }
+
+    public function getColor(): string {
+        return "<li>{$this->color}</li><br>";
+    }
+}
+
+class UnicornHotell {
+    private $unicorns = [];
+
+    function __construct($unicorns) {
+        $this->unicorns = $unicorns;
+    }
+
+    function getUnicorns() {
+        return $this->unicorns;
+    }
+
+}
+
+class RenderColor {
+    function renderColor(Unicorn $unicorn): string {
+        return $unicorn->getColor();
+    }
+}
+
+class RenderName {
+    function renderName(Unicorn $unicorn): string {
+        return $unicorn->getName();
     }
 }
 
 class RenderUnicorn {
-    public function renderMessage(Unicorn $unicorn): void {
-        echo $unicorn->getMessage();
+    function renderUnicornInfo(Unicorn $unicorn): string {
+        return $unicorn->getMessage();
     }
 }
 
-$u = new Unicorn(
+$allUnicorns = new UnicornHotell(
     [
-        "name" => "Bosse", 
-        "color" => "light green", 
-        "age" => 16
+        new Unicorn("Lisa","Pink", 12),
+        new Unicorn("Jack","Green", 15),
+        new Unicorn("Pearl","Silver", 17),
+        new Unicorn("Sofie","Blue", 19),
+        new Unicorn("Bob","Yellow", 22),
+        new Unicorn("Bosse","Orange", 32),
     ],
-    [
-        "name" => "Lisa", 
-        "color" => "blue", 
-        "age" => 15
-    ],
-    [
-        "name" => "Pearl", 
-        "color" => "white", 
-        "age" => 18
-    ],
-    [
-        "name" => "Maggie", 
-        "color" => "yellow", 
-        "age" => 20
-    ],
-    [
-        "name" => "Jack", 
-        "color" => "orange", 
-        "age" => 24
-    ]
-    );
+);
 
-$renderer = new RenderUnicorn();
-$renderer->renderMessage($u);
+$render = new RenderUnicorn();
+$renderName = new RenderName();
+$renderColor = new RenderColor();
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unicorn Hotell</title>
+</head>
+<body>
+    <h1>🦄Unicorns🦄</h1>
+    <?php
+    foreach($allUnicorns->getUnicorns() as $unicorn) {
+        echo $render->renderUnicornInfo($unicorn);
+    }
+    ?>
+ <h1>List Name</h1>
+ <ol>
+    <?php
+    foreach($allUnicorns->getUnicorns() as $unicorn) {
+        echo $renderName->renderName($unicorn);
+    }
+    ?>
+</ol>
+
+ <h1>List Color</h1>
+ <ol>
+    <?php
+    foreach($allUnicorns->getUnicorns() as $unicorn) {
+        echo $renderColor->renderColor($unicorn);
+    }
+    ?>
+</ol>
+</body>
+</html>
